779329fb250cc1f07d8e2a529095da6f24112d9c
Ingin Katsuya <96503888+Katsuya56@users.noreply.github.com>
2023-06-22 23:36:48 +0900

    Use Image View

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    val diceImages = listOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6,
+    )
+    val dice = Dice.createCubeDice()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+        val diceImage: ImageView = findViewById(R.id.imageView)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice(diceImage) }
+    }
+
+    private fun rollDice(diceImage: ImageView) {
+        // Create new Dice object with 6 sides and roll it
+
+        val diceRoll: Int = dice.roll()
+
+        // Update the screen with the dice roll
+        diceImage.setImageResource(diceImages[diceRoll])
+
+    }
+}
+
+/**
+ * Dice with a fixed number of sides.
+ */
+class Dice(private val numSides: Int) {
+    companion object {
+        fun createCubeDice(): Dice{
+            return Dice(6)
+        }
+    }
+
+    /**
+     * Do a random dice roll and return the result.
+     */
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
